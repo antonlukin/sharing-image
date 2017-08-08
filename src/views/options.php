5c9e4d1b1cb465441de964dc2b0b474324d313a8
Anton Lukin <anton@lukin.me>
2017-08-09 00:15:30 +0300

    breadcrumbs in options view

--- src/views/options.php
+++ src/views/options.php
@@ -1,14 +1,15 @@
 <div class="wrap" id="social-image-options">
-	<h1 class="wp-heading-inline">
-		<?php _e('Social Image Settings', 'social-image') ?>
-		<span class="title-count theme-count">0</span>
-	</h1>
 
-	<a href="#add-scheme" class="page-title-action" role="button"><?php _e('Add new scheme', 'social-image') ?></a>
+	<div class="social-image__catalog" style="display: none;">
+		<h1 class="wp-heading-inline">
+			<?php _e('Social Image Settings', 'social-image') ?>
+			<span class="title-count theme-count">0</span>
+		</h1>
 
-	<hr class="wp-header-end">
+		<a href="#add-scheme" class="page-title-action" role="button"><?php _e('Add new scheme', 'social-image') ?></a>
 
-	<div class="social-image__catalog">
+		<hr class="wp-header-end">
+
 		<div class="social-image__item">
 			<img class="social-image__item-image" src="https://unsplash.it/400/200" alt="" />
 
@@ -31,8 +32,37 @@
 			</h3>
 		</a>
 	</div>
+
+
+	<div class="social-image__adjust">
+ 		<h1 class="wp-heading-inline">
+			<ul class="social-image__breadcrumbs">
+				<li><a href="#catalog"><?php _e('Schemes', 'social-image') ?></a></li>
+				<li>Primary</li>
+			</ul>
+		</h1>
+
+		<a href="#add-scheme" class="page-title-action" role="button"><?php _e('Add new scheme', 'social-image') ?></a>
+
+		<hr class="wp-header-end">
+
+		<div class="social-image__form">
+ 			<h2><?php _e('Image settings', 'social-image') ?></h2>
+
+			<h2><?php _e('Logo settings', 'social-image') ?></h2>
+
+ 			<h2><?php _e('Title settings', 'social-image') ?></h2>
+		</div>
+
+		<div class="social-image__preview">
+			<img class="social-image__preview-image" src="https://unsplash.it/512/256" />
+
+			<span class="social-image__preview-caption"><?php _e('Audo-updated social image preview', 'social-image') ?></span>
+		</div>
+	</div>
+
+	<div class="clear"></div>
 <?php /*
-	<h2><?php _e('Image preview', 'social-image') ?></h2>
 	<p><?php _e('Adjust the image appearance using custom options', 'social-image') ?></p>
 
 	<div class="social-image_wrapper">
